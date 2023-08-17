fa92aa384e3e7fe470a94f159c2183712641a443
Isis Lin <qweb32114@gmail.com>
2023-08-17 09:06:47 +0800

    feat: add form control state

--- src/components/Main/Progress/StepForm.jsx
+++ src/components/Main/Progress/StepForm.jsx
@@ -1,10 +1,20 @@
 import AddressForm from "./AddressForm"
+import DeliveryForm from "./DeliveryForm"
+import PaymentForm from "./PaymentForm"
 
-function StepForm() {
+function StepForm({currentStep}) {
+    let formControl;
+    if (currentStep === 1) {
+        formControl = <AddressForm />
+    }else if (currentStep === 2) {
+        formControl = <DeliveryForm />
+    }else if (currentStep === 3) {
+        formControl = <PaymentForm />
+    }
     return (
         <>
             <form className="formControl">
-                <AddressForm />
+                {formControl}
                 <hr />
             </form>
         </>
